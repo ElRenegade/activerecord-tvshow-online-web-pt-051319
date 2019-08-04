088c281e4a953656cd413fc6b3d6d495e2e84fb8
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-04 18:26:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "shows", force: :cascade do |t|
     t.text "name"
     t.text "network"
     t.text "day"
     t.integer "rating"
+    t.string "season"
   end
 
 end
